f67ead250953cd07223c53099f2840c563be8626
Shane Janney <shanej97809@gmail.com>
2025-01-23 10:08:19 -0700

    Update app.py

---
+++ sprots-prediction-tool/src/app.py
@@ -0,0 +1,18 @@
+import sys
+# caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.insert(1, '/Users/shanejanney/Documents/GitHub/CourtWizard/sprots-prediction-tool/src/ui/menu.py')
+import main_menu
+
+class Instance:
+    def __init__(self):
+        self.menu = main_menu()
+        self.vars = None
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main___":
+    main()
